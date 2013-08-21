d438b6699c19e1f27b5a5fea97942cd527dbbb4a
Dan Tao <daniel.tao@gmail.com>
2013-08-21 15:54:12 -0700

    adding fix for WP8

---
+++ common.js
@@ -0,0 +1,10 @@
+// See https://github.com/twbs/bootstrap/pull/9171
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement("style")
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      "@-ms-viewport{width:auto!important}"
+    )
+  )
+  document.getElementsByTagName("head")[0].appendChild(msViewportStyle)
+}
